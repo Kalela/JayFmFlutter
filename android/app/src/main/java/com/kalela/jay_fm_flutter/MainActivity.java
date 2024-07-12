4c451da97804ab198047b8c2ac354a62e50eff5f
Philip Kalela <p.kalela@carepay.com>
2024-07-12 20:21:40 +0300

    Fix issues with audio service not loading

--- android/app/src/main/java/com/kalela/jay_fm_flutter/MainActivity.java
+++ android/app/src/main/java/com/kalela/jay_fm_flutter/MainActivity.java
@@ -1,6 +1,6 @@
 package com.kalela.jay_fm_flutter;
 
-import io.flutter.embedding.android.FlutterActivity;
+import com.ryanheise.audioservice.AudioServiceActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends AudioServiceActivity {
 }
